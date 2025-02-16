b6999917dab1d33c3cfb263506925f46209b9c14
Güven Fazlı <guven.fazli@gmail.com>
2025-02-17 00:44:34 +0300

    feat: for loop added with keywords break, continue

--- main.go
+++ main.go
@@ -3,51 +3,57 @@ package main
 import "fmt"
 
 func main() {
-
 	var balance float64 = 1000
 
 	fmt.Println("Welcome to GO Bank!")
-	fmt.Println("What do you want to do?")
-	fmt.Println("1. Check Balance")
-	fmt.Println("2. Deposit Money")
-	fmt.Println("3. Withdraw Money")
-	fmt.Println("4. Exit")
 
-	var choice int
-	fmt.Print("Please make a choice: ")
-	fmt.Scan(&choice)
+	for i := 0; i < 200; i++ {
+		fmt.Println("What do you want to do?")
+		fmt.Println("1. Check Balance")
+		fmt.Println("2. Deposit Money")
+		fmt.Println("3. Withdraw Money")
+		fmt.Println("4. Exit")
 
-	balanceCheck := choice == 1
-	depositMoney := choice == 2
-	withdrawMoney := choice == 3
+		var choice int
+		fmt.Print("Please make a choice: ")
+		fmt.Scan(&choice)
 
-	if balanceCheck {
-		fmt.Println("Your balance: ", balance)
-	} else if depositMoney {
-		fmt.Print("Your Deposit: ")
-		var depositValue float64
-		fmt.Scan(&depositValue)
-		if depositValue <= 0 {
-			fmt.Print("Value should be higher than 0!")
-			return
+		balanceCheck := choice == 1
+		depositMoney := choice == 2
+		withdrawMoney := choice == 3
+
+		if balanceCheck {
+			fmt.Println("Your balance: ", balance)
+		} else if depositMoney {
+			fmt.Print("Your Deposit: ")
+			var depositValue float64
+			fmt.Scan(&depositValue)
+			if depositValue <= 0 {
+				fmt.Println("Value should be higher than 0!")
+				continue
+			}
+			balance += depositValue
+			fmt.Println("Your New Balance: ", balance)
+		} else if withdrawMoney {
+			fmt.Print("Your Withdraw: ")
+			var withdrawMoney float64
+			fmt.Scan(&withdrawMoney)
+			if withdrawMoney <= 0 {
+				fmt.Println("Value should be higher than 0!")
+
+				continue
+			} else if withdrawMoney > balance {
+				fmt.Println("You can not withdraw the money that does not exist!")
+
+				continue
+			}
+
+			balance -= withdrawMoney
+			fmt.Println("Your New Balance: ", balance)
+		} else {
+			fmt.Print("Good Bye!")
+			break
 		}
-		balance += depositValue
-		fmt.Println("Your New Balance: ", balance)
-	} else if withdrawMoney {
-		fmt.Print("Your Withdraw: ")
-		var withdrawMoney float64
-		fmt.Scan(&withdrawMoney)
-		if withdrawMoney <= 0 {
-			fmt.Print("Value should be higher than 0!")
-			return
-		} else if withdrawMoney > balance {
-			fmt.Print("You can not withdraw the money that does not exist!")
-			return
-
-		}
-		balance -= withdrawMoney
-		fmt.Println("Your New Balance: ", balance)
-	} else {
-		fmt.Print("Good Bye!")
 	}
+
 }
