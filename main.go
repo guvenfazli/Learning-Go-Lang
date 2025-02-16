34ca2bc86d532d1dbdfc2ec23ef1d6bdff7dbd9d
Güven Fazlı <guven.fazli@gmail.com>
2025-02-16 23:08:36 +0300

    feat: first custom function has written.

--- main.go
+++ main.go
@@ -5,13 +5,17 @@ import "fmt"
 func main() {
 	var userValue float64
 	var infilationRate float64
-	fmt.Println("Please enter your value: ")
+	outputText("Please enter a value: ")
 	fmt.Scanf("%f", &userValue)
-	fmt.Println("Please provide an infilation rate: ")
+	outputText("Please enter an infilation rate: ")
 	fmt.Scan(&infilationRate)
 
 	var result = fmt.Sprintf("Your future value with inflation will be: %.2f", userValue*infilationRate)
 
 	fmt.Print(result)
 
+}
+
+func outputText(userText string) {
+	fmt.Print(userText)
 }
