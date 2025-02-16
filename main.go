d86e36c07c766e05c645e84fd0d13d0f9198a546
Güven Fazlı <guven.fazli@gmail.com>
2025-02-16 23:42:10 +0300

    feat: excersise completed successfully

--- main.go
+++ main.go
@@ -3,25 +3,43 @@ package main
 import "fmt"
 
 func main() {
-	var revenue float64
-	var expenses float64
-	var taxRate float64
 
-	fmt.Print("Revenue: ")
-	fmt.Scan(&revenue)
+	enteredRevenue, enteredExpenses, enteredTaxRate := gatherUserValues("Revenue: ", "Expenses: ", "Tax Rate: ")
 
-	fmt.Print("Expenses: ")
-	fmt.Scan(&expenses)
-
-	fmt.Print("Tax Rate: ")
-	fmt.Scan(&taxRate)
-
-	ebt := revenue - expenses
-	profit := ebt * (1 - taxRate/100)
-	ratio := ebt / profit
+	ebt, profit, ratio := calculateWithValues(enteredRevenue, enteredExpenses, enteredTaxRate)
 
 	fmt.Println(ebt)
 	fmt.Println(profit)
 	fmt.Println(ratio)
 
+}
+
+func gatherUserValues(revenueMessage, expensesMessage, taxRateMessage string) (enteredRevenue, enteredExpenses, enteredTaxRate float64) {
+
+	var revenue float64
+	var expenses float64
+	var taxRate float64
+
+	fmt.Print(revenueMessage)
+	fmt.Scan(&revenue)
+	enteredRevenue = revenue
+
+	fmt.Print(expensesMessage)
+	fmt.Scan(&expenses)
+	enteredExpenses = expenses
+
+	fmt.Print(taxRateMessage)
+	fmt.Scan(&taxRate)
+	enteredTaxRate = taxRate
+
+	return enteredRevenue, enteredExpenses, enteredTaxRate
+
+}
+
+func calculateWithValues(revenue, expenses, taxRate float64) (ebt, profit, ratio float64) {
+	ebt = revenue - expenses
+	profit = ebt * (1 - taxRate/100)
+	ratio = ebt / profit
+
+	return ebt, profit, ratio
 }
