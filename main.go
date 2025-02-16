2758089dc3782240f95ff1a64393964aca3114ed
Güven Fazlı <guven.fazli@gmail.com>
2025-02-16 23:22:53 +0300

    feat: returning multiple values completed

--- main.go
+++ main.go
@@ -4,18 +4,33 @@ import "fmt"
 
 func main() {
 	var userValue float64
+	var userYear int
 	var infilationRate float64
+
 	outputText("Please enter a value: ")
 	fmt.Scanf("%f", &userValue)
+	outputText("Please provide years: ")
+	fmt.Scan(&userYear)
 	outputText("Please enter an infilation rate: ")
 	fmt.Scan(&infilationRate)
 
-	var result = fmt.Sprintf("Your future value with inflation will be: %.2f", userValue*infilationRate)
+	withInfilation, withoutInfilation := calculateFutureValue(userValue, infilationRate, userYear)
 
-	fmt.Print(result)
+	/* 	var result = fmt.Sprintf("Your future value with inflation will be: %.2f", userValue*infilationRate)
+	 */
+
+	fmt.Println("Your value with Infilation: ", withInfilation)
+	fmt.Print("Your value without Infilation: ", withoutInfilation)
 
 }
 
 func outputText(userText string) {
 	fmt.Print(userText)
+}
+
+func calculateFutureValue(userValue, infilationRate float64, year int) (float64, float64) {
+	futureValueWithoutInfilation := userValue * float64(year)
+	futureValueWithInfilation := (userValue * float64(year)) / infilationRate
+
+	return futureValueWithInfilation, futureValueWithoutInfilation
 }
