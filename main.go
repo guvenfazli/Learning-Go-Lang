c594ecaf39c13d6edfbdb140fb447cfe1fea0d44
Güven Fazlı <guven.fazli@gmail.com>
2025-02-19 02:57:13 +0300

    chore: first 3rd party package installed

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"example.com/learningGO/fileOps"
+	"github.com/Pallinder/go-randomdata"
 )
 
 const accountBalanceFile = "balance.txt"
@@ -19,6 +20,7 @@ func main() {
 	}
 
 	fmt.Println("Welcome to Go Bank!")
+	fmt.Println("Reach to us 24/7: ", randomdata.PhoneNumber())
 
 	for {
 		presentOptions()
