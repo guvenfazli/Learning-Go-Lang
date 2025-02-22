0826bbfc7cb8fdf34f98e6b4124f55bbf7aafcea
Güven Fazlı <guven.fazli@gmail.com>
2025-02-22 03:33:24 +0300

    feat: creating first variable as struct completed.

--- pointers.go
+++ pointers.go
@@ -2,38 +2,69 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
-type User struct {
+/* type User struct {
 	firstName string
 	lastName  string
 	birthDate string
 	createdAt time.Time
+} */
+
+type CreatedItem struct {
+	title  string
+	price  string
+	isSold string
 }
 
 func main() {
-	userFirstName := getUserData("Please enter your first name: ")
-	userLastName := getUserData("Please enter your last name: ")
-	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
 
-	var appUser User
+	itemTitle := createItem()
+	itemPrice := createItem()
+	itemSold := createItem()
 
-	appUser = User{
-		userFirstName,
-		userLastName,
-		userBirthdate,
-		time.Now(),
+	addedItem := CreatedItem{
+		itemTitle,
+		itemPrice,
+		itemSold,
 	}
 
-	// ... do something awesome with that gathered data!
+	renderItem(addedItem)
 
-	fmt.Println(userFirstName, userLastName, userBirthdate)
+	/* 	userFirstName := getUserData("Please enter your first name: ")
+	   	userLastName := getUserData("Please enter your last name: ")
+	   	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+
+	   	var appUser User
+
+	   	appUser = User{
+	   		userFirstName,
+	   		userLastName,
+	   		userBirthdate,
+	   		time.Now(),
+	   	}
+
+	   	// ... do something awesome with that gathered data!
+
+	   	fmt.Println(userFirstName, userLastName, userBirthdate) */
 }
 
-func getUserData(promptText string) string {
+func createItem() string {
+	var enteredValue string
+	fmt.Println("Please put a name for your item: ")
+	fmt.Scan(&enteredValue)
+	return enteredValue
+}
+
+func renderItem(itm CreatedItem) {
+	fmt.Println("Your items title: ", itm.title)
+	fmt.Println("Your items price: ", itm.price)
+	fmt.Println("Is your item sold: ", itm.isSold)
+}
+
+/* func getUserData(promptText string) string {
 	fmt.Print(promptText)
 	var value string
 	fmt.Scan(&value)
 	return value
-}
+} */
