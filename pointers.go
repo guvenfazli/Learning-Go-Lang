c2940b8a6aa343c212ad7ff5d125b1cb37ff4936
Güven Fazlı <guven.fazli@gmail.com>
2025-02-22 03:25:48 +0300

    feat: first step of structs completed

--- pointers.go
+++ pointers.go
@@ -1,23 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func main() {
-	age := 32 // regular variable
-
-	agePointer := &age
-
-	var name string
-
-	userName := &name
-	fmt.Println(*userName)
-
-	fmt.Println("Age:", agePointer)
-	adultYears := getAdultYears(*agePointer)
-	fmt.Println(adultYears)
-
+type User struct {
+	firstName string
+	lastName  string
+	birthDate string
+	createdAt time.Time
 }
 
-func getAdultYears(age int) int {
-	return age - 18
+func main() {
+	userFirstName := getUserData("Please enter your first name: ")
+	userLastName := getUserData("Please enter your last name: ")
+	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+
+	var appUser User
+
+	appUser = User{
+		userFirstName,
+		userLastName,
+		userBirthdate,
+		time.Now(),
+	}
+
+	// ... do something awesome with that gathered data!
+
+	fmt.Println(userFirstName, userLastName, userBirthdate)
+}
+
+func getUserData(promptText string) string {
+	fmt.Print(promptText)
+	var value string
+	fmt.Scan(&value)
+	return value
 }
