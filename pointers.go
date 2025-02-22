f4f5e56c76f53ef32679a408f9d655751eb88a8d
Güven Fazlı <guven.fazli@gmail.com>
2025-02-22 06:02:01 +0300

    feat: user log in and create account functions created, cases created

--- pointers.go
+++ pointers.go
@@ -1,70 +1,111 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-/* type User struct {
-	firstName string
-	lastName  string
-	birthDate string
-	createdAt time.Time
-} */
-
-type CreatedItem struct {
-	title  string
-	price  string
-	isSold string
+type UserAccount struct {
+	name      string
+	surname   string
+	age       int64
+	email     string
+	password  string
+	isMarried bool
 }
 
 func main() {
 
-	itemTitle := createItem()
-	itemPrice := createItem()
-	itemSold := createItem()
+	var createdAccount UserAccount
+	var accountCreated bool
+	usersChoice := greetUserAndNavigate()
 
-	addedItem := CreatedItem{
-		itemTitle,
-		itemPrice,
-		itemSold,
+	switch usersChoice {
+	case 1:
+		{
+			fmt.Print("Login!")
+
+		}
+	case 2:
+		{
+			fmt.Print("Create an account!")
+			createdAccount, accountCreated := createAnAccount()
+
+		}
+	default:
+		{
+			fmt.Print("Unknown command selected! App is shutting down.")
+			return
+		}
 	}
 
-	renderItem(addedItem)
+	if accountCreated {
 
-	/* 	userFirstName := getUserData("Please enter your first name: ")
-	   	userLastName := getUserData("Please enter your last name: ")
-	   	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+	}
 
-	   	var appUser User
-
-	   	appUser = User{
-	   		userFirstName,
-	   		userLastName,
-	   		userBirthdate,
-	   		time.Now(),
-	   	}
-
-	   	// ... do something awesome with that gathered data!
-
-	   	fmt.Println(userFirstName, userLastName, userBirthdate) */
 }
 
-func createItem() string {
-	var enteredValue string
-	fmt.Println("Please put a name for your item: ")
-	fmt.Scan(&enteredValue)
-	return enteredValue
+func greetUserAndNavigate() int64 {
+	var accountOption int64
+	fmt.Println("Welcome!")
+	fmt.Println("In order to use my application, please log in or create an account!")
+	fmt.Println("1. I do not have an account: ")
+	fmt.Println("2. I do have an account: ")
+	fmt.Print("Please choose an option: ")
+	fmt.Scan(&accountOption)
+	return accountOption
 }
 
-func renderItem(itm CreatedItem) {
-	fmt.Println("Your items title: ", itm.title)
-	fmt.Println("Your items price: ", itm.price)
-	fmt.Println("Is your item sold: ", itm.isSold)
+func createAnAccount() (UserAccount, bool) {
+	var userName string
+	var userSurname string
+	var userAge int64
+	var userEmail string
+	var userPassword string
+	var userIsMarried bool
+
+	var createdAccount UserAccount
+
+	fmt.Println("Your name: ")
+	fmt.Scan(&userName)
+	fmt.Println("Your surname: ")
+	fmt.Scan(&userSurname)
+	fmt.Println("Your age: ")
+	fmt.Scan(&userAge)
+	fmt.Println("Your email: ")
+	fmt.Scan(&userEmail)
+	fmt.Println("Your password: ")
+	fmt.Scan(&userPassword)
+	fmt.Println("Are you married? ")
+	fmt.Scan(&userIsMarried)
+
+	createdAccount = UserAccount{
+		userName,
+		userSurname,
+		userAge,
+		userEmail,
+		userPassword,
+		userIsMarried,
+	}
+	accountCreated := true
+	return createdAccount, accountCreated
 }
 
-/* func getUserData(promptText string) string {
-	fmt.Print(promptText)
-	var value string
-	fmt.Scan(&value)
-	return value
-} */
+func logInAccount(createdAccount UserAccount, createdAccountEmail, createdAccountPassword string) string {
+	var userEmail string
+	var userPassword string
+
+	fmt.Println("Please enter your fresh created accounts email: ")
+	fmt.Scan(&userEmail)
+	fmt.Println("Password Please: ")
+	fmt.Scan(&userPassword)
+
+	if userEmail != createdAccountEmail || userPassword != createdAccountPassword {
+		errorMessage := "Username or password is wrong, please restart the app and try again!"
+		return errorMessage
+	} else {
+
+		greetMessage := "Welcome!"
+
+		return greetMessage
+
+	}
+
+}
