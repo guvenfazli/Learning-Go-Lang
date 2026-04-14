99ccf30959e1c7638cf649ce6909fab6e29035cd
Güven Fazlı <guven.fazli@gmail.com>
2026-04-14 13:09:14 +0300

    if else and alternative decleration checked

--- investment.go
+++ investment.go
@@ -7,8 +7,16 @@ import (
 
 func main() {
 	var investmentAmount float64 = 1000
-	var expectedReturnRate = 5.5
+	expectedReturnRate := 5.5
 	var years float64 = 10
+
+	if years > 41 {
+		fmt.Println("YES!")
+	} else if years > 43 {
+		fmt.Println("YES!")
+	} else {
+		fmt.Println("Woohoo!")
+	}
 
 	var futureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	fmt.Print(futureValue)
