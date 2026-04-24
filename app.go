8f682a99af7554246f9629f400c0de729fbfdf6c
Güven Fazlı <guven.fazli@gmail.com>
2026-04-24 17:06:02 +0300

    feat: Printf() learnt

--- app.go
+++ app.go
@@ -5,25 +5,12 @@ import (
 )
 
 func main() {
+	firstValue := 1628.8946267774415
+	secondValue := 1272.4898790181621
 
-	mySalary := 1000
-	isItDone := 0
-	var expenses int
+	//	fmt.Println("First Value:", firstValue)
+	//	fmt.Println("Second Value:", secondValue)
 
-	fmt.Print("Your total expenses: ")
-	fmt.Scan(&expenses)
-	fmt.Println("Was it your last expense?")
-	fmt.Println("1) Yes")
-	fmt.Println("2) No")
-	fmt.Scan(&isItDone)
-	if isItDone == 2 {
-		fmt.Println("Oh god...")
-		return
-	} else {
-		fmt.Println("Your salary after promotion: ", mySalary)
-		fmt.Print("Please enter an imaginary salary: ")
-		fmt.Scan(&mySalary)
-		fmt.Println("With that, your total revenue will be: ", mySalary-expenses)
-	}
+	fmt.Printf("First Value: %v\nSecond Value: %v\n", firstValue, secondValue)
 
 }
