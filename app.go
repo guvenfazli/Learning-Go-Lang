ce6e3c6c1c31978c6b62b8034c04efef0f2053fc
Güven Fazlı <guven.fazli@gmail.com>
2026-04-24 16:27:39 +0300

    revision started

--- app.go
+++ app.go
@@ -1,24 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	fmt.Println("Welcome to the profit calculator.")
-
-	var revenue float64
-	var expenses float64
-	var taxRate float64
-
-	fmt.Print("Your Revenue:")
-	fmt.Scan(&revenue)
-	fmt.Print("Your Expenses:")
-	fmt.Scan(&expenses)
-	fmt.Print("Estimated Tax Rate:")
-	fmt.Scan(&taxRate)
-
-	fmt.Println("Your earnings before tax: ", revenue-expenses)
-	fmt.Println("Your earnings after tax: ", (revenue-expenses)/taxRate)
 
 }
