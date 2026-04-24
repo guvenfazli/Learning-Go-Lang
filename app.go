8b7e1c37832ac539f41702b357e4af6834d664ec
Güven Fazlı <guven.fazli@gmail.com>
2026-04-24 17:58:45 +0300

    feat: Sprint learnt. Key difference is, while Printf only formats and puts the output to terminal, Sprint returns a string value w it.

--- app.go
+++ app.go
@@ -8,9 +8,13 @@ func main() {
 	firstValue := 1628.8946267774415
 	secondValue := 1272.4898790181621
 
+	formattedFV := fmt.Sprintf("First Value: %.1f\n", firstValue)
+	formattedSV := fmt.Sprintf("Second Value: %.1f\n", secondValue)
+	fmt.Printf("First Type: %v\nSecond Type: %v\n", formattedFV, formattedSV)
+
 	//	fmt.Println("First Value:", firstValue)
 	//	fmt.Println("Second Value:", secondValue)
 
-	fmt.Printf("First value: %.82f\nSecond Value: %.92f", firstValue, secondValue)
+	// fmt.Printf("First value: %.82f\nSecond Value: %.92f", firstValue, secondValue)
 
 }
