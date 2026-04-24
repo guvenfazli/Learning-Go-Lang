5dc78e91c05b8d2c6515208d299ab5c7fc83e5f9
Güven Fazlı <guven.fazli@gmail.com>
2026-04-24 16:32:18 +0300

    feat: test value imported

--- app.go
+++ app.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
+
+	mySalary := 1000
+	var expenses int
+	const testValue int = 900
+
+	fmt.Println("Your total expenses: ")
+	fmt.Scan(&expenses)
+	fmt.Println("Your current salary is: ", mySalary)
+	fmt.Print("Please enter an imaginary salary: ")
+	fmt.Scan(&mySalary)
+	fmt.Println("With that, your total revenue will be: ", mySalary-expenses)
 
 }
